f013be4587fda2e7d8b3faed714d971da54921d8
Sithija <your-email@example.com>
2026-06-06 11:19:43 +0530

    database configurations

---
+++ src/main/java/org/example/config/DBManager.java
@@ -0,0 +1,40 @@
+package org.example.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class DBManager {
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+    private static Properties properties = new Properties();
+
+
+    static {
+        try (InputStream input = DBManager.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+            } else {
+                properties.load(input);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DBManager() {}
+
+    public static MongoDatabase getDatabase() {
+        if (mongoClient == null) {
+            String uri = properties.getProperty("mongodb.uri");
+            String dbName = properties.getProperty("mongodb.database");
+
+            mongoClient = MongoClients.create(uri);
+            database = mongoClient.getDatabase(dbName);
+        }
+        return database;
+    }
+}
